d912b7d735e12ab8ae389aa2ea3f26868168d2eb
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-11-04 22:09:55 -0300

    atts

---
+++ mobile/src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { TouchableOpacity, Text } from "react-native";
+
+const ButtonComponent = props => (
+  <TouchableOpacity>
+    <Text>OPA</Text>
+  </TouchableOpacity>
+);
+
+export default ButtonComponent;
---
+++ mobile/src/pages/UserSignIn/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import { View, Text } from "react-native";
+import ButtonComponent from "../../components/Button";
+
+// import { Container } from './styles';
+
+export default class UserSignIn extends Component {
+  render() {
+    return (
+      <View>
+        <Text>OI</Text>
+      </View>
+    );
+  }
+}
