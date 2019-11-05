96466f066a4d8e437cf8e1d1d2303bb00c989b8a
Vinicius Contini <viniciuscontini@MacBook-Pro-de-Vinicius.local>
2019-11-05 10:59:36 -0300

    criando componente TextInput e adicionando o style

---
+++ mobile/src/components/TextInput/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { TextInput } from "./styles";
+
+const TextInputComponent = props => (
+  <View>
+    <TextInput placeholder="Email" />
+  </View>
+);
+
+TextInputComponent.defaultsProps = {
+  name: "Digite seu email"
+};
+
+export default TextInputComponent;
---
+++ mobile/src/components/TextInput/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const TextInput = styled.TextInput`
+  padding: 11px;
+  padding-left: 15;
+  padding-right: 15;
+  border-radius: 5;
+  background-color: #f5f5f5;
+  align-self: stretch;
+  font-size: 16;
+  width: 300;
+  justify-content: center;
+  margin-bottom: 10;
+`;
