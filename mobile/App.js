73e01b7eab6239da986a7553ccbe8e2fcdabcba2
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-11-05 22:02:54 -0300

    limpeza codigo

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,3 @@
-// import React from "react";
-// import UserSignIn from "./src/pages/UserSignIn";
-
-// const App = () => <UserSignIn />;
-
-// export default App;
-
 import React from "react";
 import Routes from "./src/routes";
 
