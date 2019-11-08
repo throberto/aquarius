b028835faa368c3b8caefae655d578b6e31e40e2
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2019-11-08 00:59:35 -0300

    Implementado state e escopo das funções para realizar a requisição para a API.

--- mobile/src/pages/UserResetPassword/index.js
+++ mobile/src/pages/UserResetPassword/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, Alert } from "react-native";
 
 StatusBar.setBackgroundColor("#1793F9");
 StatusBar.setBarStyle("light-content");
@@ -22,6 +22,22 @@ export default class UserResetPassword extends Component {
     }
   };
 
+  state = {
+    email: ""
+  };
+
+  handleEmailChange = email => {
+    this.setState({ email });
+  };
+
+  handleResetPasswordPress = async () => {
+    if (this.state.email.length === 0) {
+      Alert.alert("Atenção", "Informe seu e-mail.");
+    } else {
+      Alert.alert("Bem Vindo", "E-mail: " + this.state.email);
+    }
+  };
+
   render() {
     return (
       <Container behavior="padding">
@@ -31,8 +47,14 @@ export default class UserResetPassword extends Component {
             placeholder="Email"
             keyboardType="email-address"
             borderRadius={30}
+            onChangeText={this.handleEmailChange}
+            value={this.setState.email}
           />
-          <ButtonComponent name="Enviar" borderRadius={30} />
+          <ButtonComponent
+            name="Enviar"
+            borderRadius={30}
+            onPress={() => this.handleResetPasswordPress()}
+          />
         </View>
       </Container>
     );
