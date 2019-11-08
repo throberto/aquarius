e12b3f2ca1e06994fde5b6bf16db1ea4dc5a479e
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2019-11-08 01:00:24 -0300

    Implementado state e escopo das funções para realizar a requisição para a API.

--- mobile/src/pages/HelpRegister/index.js
+++ mobile/src/pages/HelpRegister/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { KeyboardAvoidingView, StatusBar } from "react-native";
+import { KeyboardAvoidingView, StatusBar, Alert } from "react-native";
 
 import ButtonComponent from "../../components/Button";
 import TextInputComponent from "../../components/TextInput";
@@ -21,17 +21,37 @@ export default class HelpRegister extends Component {
     }
   };
 
+  state = {
+    email: ""
+  };
+
+  handleEmailChange = email => {
+    this.setState({ email });
+  };
+
+  handleHelpRegisterPress = async () => {
+    if (this.state.email.length === 0) {
+      Alert.alert("Atenção", "Informe seu e-mail.");
+    } else {
+      Alert.alert("Bem Vindo", "E-mail: " + this.state.email);
+    }
+  };
+
   render() {
     return (
       <Container behavior="padding">
         <View behavior="padding">
-
           <TextInputComponent
             placeholder="Email"
             keyboardType="email-address"
+            onChangeText={this.handleEmailChange}
+            value={this.setState.email}
           />
 
-          <ButtonComponent title="Enviar" />
+          <ButtonComponent
+            title="Enviar"
+            onPress={() => this.handleHelpRegisterPress()}
+          />
         </View>
       </Container>
     );
