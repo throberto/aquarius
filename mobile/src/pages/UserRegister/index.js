8473e49e7869a6fb44efd8cd431c72649123dc0e
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2019-11-08 00:58:54 -0300

    Implementado state e escopo das funções para realizar a requisição para a API.

--- mobile/src/pages/UserRegister/index.js
+++ mobile/src/pages/UserRegister/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { KeyboardAvoidingView, StatusBar } from "react-native";
+import { KeyboardAvoidingView, StatusBar, Alert } from "react-native";
 
 import ButtonComponent from "../../components/Button";
 import TextInputComponent from "../../components/TextInput";
@@ -22,23 +22,74 @@ export default class UserRegister extends Component {
     }
   };
 
+  state = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  handleNameChange = name => {
+    this.setState({ name });
+  };
+
+  handleEmailChange = email => {
+    this.setState({ email });
+  };
+
+  handlePasswordChange = password => {
+    this.setState({ password });
+  };
+
+  handleRedisterPress = async () => {
+    if (
+      this.state.name.length === 0 ||
+      this.state.email.length === 0 ||
+      this.state.password.length === 0
+    ) {
+      Alert.alert("Atenção", "Preencha todos os campo.");
+    } else {
+      Alert.alert(
+        "Bem Vindo!",
+        "Nome: " +
+          this.state.name +
+          " E-mail: " +
+          this.state.email +
+          " Senha: " +
+          this.state.password
+      );
+    }
+  };
+
   render() {
     return (
       <Container behavior="padding">
         <LogoComponent />
         <View behavior="padding">
-          <TextInputComponent placeholder="Nome" borderRadius={30} />
+          <TextInputComponent
+            placeholder="Nome"
+            borderRadius={30}
+            onChangeText={this.handleNameChange}
+            value={this.state.name}
+          />
           <TextInputComponent
             placeholder="Email"
             keyboardType="email-address"
             borderRadius={30}
+            onChangeText={this.handleEmailChange}
+            value={this.state.email}
           />
           <TextInputComponent
             placeholder="Senha"
             secureTextEntry={true}
             borderRadius={30}
+            onChangeText={this.handlePasswordChange}
+            value={this.state.password}
           />
-          <ButtonComponent title="Cadastrar" borderRadius={30} />
+          <ButtonComponent
+            title="Cadastrar"
+            borderRadius={30}
+            onPress={() => this.handleRedisterPress()}
+          />
         </View>
       </Container>
     );
