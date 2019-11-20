7d3464e73f8ced6a0627baeeddd4f9345898bb24
Helio Rodrigues <hcrodrigues.filho@gmail.com>
2019-11-19 22:49:04 -0300

    Iniciando criacao da peixedex/ fishlist

--- mobile/src/pages/FishList/index.js
+++ mobile/src/pages/FishList/index.js
@@ -1,11 +1,44 @@
-import React, { Component } from "react";
-
-import { View } from "react-native";
-
-// import { Container } from './styles';
-
-export default class FishList extends Component {
+import React from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+class App extends React.Component {
+  state = {
+    data: [
+      { id: "00", name: "Peixe 1 " },
+      { id: "01", name: "Peixe 2" },
+      { id: "02", name: "Peixe 3" },
+      { id: "03", name: "Peixe 4" }
+    ]
+  };
   render() {
-    return <View />;
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={item => item.id}
+          numColumns={3}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.item}>
+                <Text style={styles.text}>{item.name}</Text>
+              </View>
+            );
+          }}
+        />
+      </SafeAreaView>
+    );
   }
 }
+const styles = StyleSheet.create({
+  item: {
+    alignItems: "center",
+    backgroundColor: "#dcda48",
+    flexGrow: 1,
+    margin: 4,
+    padding: 20,
+    flexBasis: 0,
+  },
+  text: {
+    color: "#333333"
+  }
+});
+export default App;
