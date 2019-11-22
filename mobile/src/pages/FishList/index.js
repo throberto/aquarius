c0949b296184a43fba43cdbb79187f1b05430fcf
Vinicius <vinicius@MacBook-Pro-de-Vinicius.local>
2019-11-22 19:43:08 -0300

    ajustes na tela FishList e conectando a API

--- mobile/src/pages/FishList/index.js
+++ mobile/src/pages/FishList/index.js
@@ -1,48 +1,17 @@
-import React from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
-class App extends React.Component {
-  state = {
-    data: [
-      { id: "00", name: "Peixe 1 " },
-      { id: "01", name: "Peixe 2" },
-      { id: "02", name: "Peixe 3" },
-      { id: "03", name: "Peixe 4" },
-      { id: "04", name: "Peixe 5" },
-      { id: "05", name: "Peixe 6" },
-      { id: "06", name: "Peixe 7" },
-      { id: "07", name: "Peixe 8" }
-    ]
-  };
+import React, { Component } from "react";
+
+import FishListComponent from "../../components/FishList";
+
+import { Container, View } from "./styles";
+
+export default class FishList extends Component {
   render() {
     return (
-      <SafeAreaView>
-        <FlatList
-          data={this.state.data}
-          keyExtractor={item => item.id}
-          numColumns={3}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.item}>
-                <Text style={styles.text}>{item.name}</Text>
-              </View>
-            );
-          }}
-        />
-      </SafeAreaView>
+      <Container>
+        <View>
+          <FishListComponent />
+        </View>
+      </Container>
     );
   }
 }
-const styles = StyleSheet.create({
-  item: {
-    alignItems: "center",
-    backgroundColor: "#dcda48",
-    flexGrow: 1,
-    margin: 4,
-    padding: 20,
-    flexBasis: 0
-  },
-  text: {
-    color: "#333333"
-  }
-});
-export default App;
